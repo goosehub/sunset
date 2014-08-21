d7d58c82830dcc0d79dc3990f6d7fc194e3e09fc
Goose <goosemixes@gmail.com>
2014-08-20 23:38:32 -0400

    Plugin Easing
    Several updates, including the easing plugin

--- demoscript.js
+++ demoscript.js
@@ -1,23 +1,31 @@
 $(document).ready(function() {
 
-	$("body").fadeIn(8000);
-
-	$("#sun").fadeIn(8000);
-
-	$( "#mountains" ).delay( 2000 ).animate({
-	    marginTop: "-25%",
-	}, 6000 );
+	$( "body" ).animate({
+		    opacity: 1.0,
+		}, 4000, "easeInOutQuad" );
 
 	$( "#sun" ).animate({
-	    marginBottom: "-300px",
-	}, 8000, "linear" );
+		    opacity: 1.0,
+		}, 6000, "linear" );
 
-	$( "#sunset, #sun" ).delay( 10000 ).animate({
-	    opacity: 0.0,
-	}, 5000 );
+	$( "#mountains" ).delay( 4000 ).animate({
+	    marginTop: "-25%",
+	}, 3000, "easeOutExpo");
 
-	$( "#space" ).delay( 14000 ).animate({
-	    opacity: 4.0,
-	}, 20000 );
+	$('#sun').on("click", function(){
+
+		$( "#sun" ).animate({
+		    marginBottom: "-300px",
+		}, 8000, "easeInSine" );
+
+		$( "#sunset, #sun" ).animate({
+		    opacity: 0.0,
+		}, 7000 , "easeInCubic" );
+
+		$( "#space" ).delay( 4000 ).animate({
+		    opacity: 1.0,
+		}, 10000, "easeInCubic" );
+
+	});
 
 });
